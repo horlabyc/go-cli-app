8faaea3eeda0c50df5dfc801e3d44dd5ad6b8821
Sulaiman Olaosebikan <olabisisulaiman@gmail.com>
2021-10-22 20:30:03 +0100

    full app done

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	path := flag.String("path", "myapp.log", "The path to the logg file to analyse")
+	level := flag.String("level", "ERROR", "Log level to serach for. Options are DEBUG, INFO, ERROR and WARNING")
+
+	flag.Parse()
+
+	f, err := os.Open(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	for {
+		s, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if strings.Contains(s, *level) {
+			fmt.Println(s)
+		}
+	}
+}
